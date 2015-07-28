b6660aa7cab1dc1379f092c0328ce191ec593ac1
lazouche <info@overio.com>
2015-07-28 12:21:21 -0400

    changed POW version and Protocol

--- src/version.h
+++ src/version.h
@@ -28,14 +28,14 @@ static const int DATABASE_VERSION = 80000;
 // PoSV
 // nTime field added to CTransaction
 // vchBlockSig field added to CBlock  1000000
-static const int POW_CLIENT_VERSION = 80701;
+static const int POW_CLIENT_VERSION = 80722;
 static const int POW_PROTOCOL_VERSION = 70002;
 static const int POW_TX_VERSION = 1;
 static const int POW_BLOCK_VERSION = 2;
 
 // network protocol versioning
 //
-static const int PROTOCOL_VERSION = 70002;
+static const int PROTOCOL_VERSION = 80000;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
